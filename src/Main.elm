36352591ecf5372c2f6cd678b37308951ad412e7
Timothy Davis <tmthydvs@gmail.com>
2024-07-23 01:43:17 +0930

    Build with Vite

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,9 @@ module Main exposing (main)
 
 import Browser
 import Elements exposing (Element, generate)
-import Html
+import Html exposing (..)
+import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
 import Random
 
 
@@ -15,46 +17,85 @@ main =
     Browser.element { init = init, view = view, update = update, subscriptions = subscriptions }
 
 
-type Model
+type State
     = Waiting
     | Sequence (List Element)
 
 
+type alias Model =
+    { state : State
+    , count : Int
+    }
+
+
 type Msg
     = NewSequence (List Element)
+    | Regenerate
+    | SetCount Int
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Waiting, Random.generate NewSequence (generate 20) )
+    let
+        initialCount =
+            30
+    in
+    ( Model Waiting initialCount, Random.generate NewSequence (generate initialCount) )
 
 
 view model =
-    case model of
+    case model.state of
         Waiting ->
-            Html.div [] [ Html.text "Waiting for sequence to generate" ]
+            main_ [] [ text "Waiting for sequence to generate" ]
 
         Sequence sequence ->
-            showSequence sequence
+            main_ []
+                [ showControl model
+                , showSequence sequence
+                ]
 
 
 update msg model =
     case msg of
         NewSequence sequence ->
-            ( Sequence sequence, Cmd.none )
+            ( { model | state = Sequence sequence }, Cmd.none )
+
+        Regenerate ->
+            ( model, Random.generate NewSequence (generate model.count) )
+
+        SetCount count ->
+            ( { model | count = clamp 2 99 count }, Cmd.none )
 
 
 subscriptions _ =
     Sub.none
 
 
-showSequence : List Element -> Html.Html msg
+
+-- FINE GRAINED COMPONENTS
+
+
+showControl : Model -> Html Msg
+showControl { count } =
+    div [ class "controls" ]
+        [ button [ class "generate", onClick Regenerate ] [ text "Generate!" ]
+        , div [ class "count" ]
+            [ button [ onClick (SetCount (count - 10)) ] [ text "-10" ]
+            , button [ onClick (SetCount (count - 1)) ] [ text "-" ]
+            , span [] [ text "Count: ", pre [] [ text (String.fromInt count |> String.padLeft 2 ' ') ] ]
+            , button [ onClick (SetCount (count + 1)) ] [ text "+" ]
+            , button [ onClick (SetCount (count + 10)) ] [ text "+10" ]
+            ]
+        ]
+
+
+showSequence : List Element -> Html Msg
 showSequence sequence =
-    Html.ul [] (List.map showMove sequence)
+    ol [ class "sequence" ] (List.map showElement sequence)
 
 
-showMove : Element -> Html.Html msg
-showMove m =
-    Html.li []
-        [ Html.text (Elements.displayName m True)
+showElement : Element -> Html Msg
+showElement m =
+    li [ class "sequence-item" ]
+        [ text (Elements.displayName m True)
         ]
